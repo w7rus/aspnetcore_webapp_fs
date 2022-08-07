87e5347976154f1b9b8194ef258a5de1ed0a3448
W7RUS-PC\w7rus <bragingrigory@gmail.com>
2022-08-07 15:37:51 +0700

    no message

--- Common/Attributes/DisableFormValueModelBindingAttribute.cs
+++ Common/Attributes/DisableFormValueModelBindingAttribute.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Common.Attributes;
 
